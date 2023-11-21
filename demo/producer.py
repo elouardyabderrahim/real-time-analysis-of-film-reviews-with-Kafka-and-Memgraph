f28063e4cb8123a326293d81949acd069b7e56b7
Sefdine Nassuf <74672023+Sefdine@users.noreply.github.com>
2023-11-21 16:45:54 +0100

    feat : add demo producer

---
+++ demo/producer.py
@@ -0,0 +1,26 @@
+from confluent_kafka import Producer, KafkaError
+from neo4j import GraphDatabase
+
+# Kafka configuration
+kafka_config = {
+    'bootstrap.servers': 'localhost:9092'
+}
+
+# Kafka producer
+def produce_message(producer, topic, message):
+    producer.produce(topic, value=message)
+    producer.flush()
+
+# Main function
+def main():
+    topic = 'demo_topic'
+    message_to_send = 'Hello, Kafka and Memgraph!'
+
+    producer = Producer(kafka_config)
+
+    # Produce a message to Kafka
+    produce_message(producer, topic, message_to_send)
+    print(f"Produced message to {topic}: {message_to_send}")
+
+if __name__ == "__main__":
+    main()
