81f160e37f64deeecadb4234939a274eacb8a6a1
amin <aminelfaquiri0@gmail.com>
2023-11-22 17:02:02 +0100

    feat : #8 create the other api app for get movies and users  data by id

--- api/api_creator.py
+++ api/api_creator.py
@@ -11,5 +11,27 @@ with open('movies_rating.json') as f:
 def get_movies():
     return jsonify(json_data)
 
+@app.route('/api/moviesId', methods=['GET'])
+def get_all_movie_ids():
+    unique_movie_ids = set()
+    for item in json_data:
+        movie_id = item["movie"].get("movieId")
+        if movie_id:
+            unique_movie_ids.add(movie_id)
+    
+    return jsonify(list(unique_movie_ids))
+
+
+@app.route('/api/usersId', methods=['GET'])
+def get_all_user_ids():
+    unique_user_ids = set()
+    for item in json_data:
+        user_id = item.get("userId")
+        if user_id:
+            unique_user_ids.add(user_id)
+    
+    return jsonify(list(unique_user_ids))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
